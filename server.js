70b6a8533ebcc27b0f62e8f708dfaff3a48fd4f9
Gage LaVergne <gagelavergne@Gages-MacBook-Pro.local>
2017-12-07 00:36:52 -0600

    still working on glitches

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 var express = require("express");
-var bodyParser = require("bosy-parser");
+var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var PORT = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(__dirname + "/public"));
 
-app.use(bodyParsser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(methodOverride("_method"));
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-var routes = require("./controller/burgersController.js");
+var routes = require("./controllers/burgers_controllers.js");
 
 app.use("/", routes);
 app.use("/update", routes);
-app.use("?create", routes);
+app.use("/create", routes);
 
-app.listen(Port, function() {
-	console.log("Listening on port", PORT);
+app.listen(PORT, function() {
+	console.log("Listening on port: ", PORT);
 });
